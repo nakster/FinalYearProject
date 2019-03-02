d85a2887b53d8700945cc8bbd4cae4f141780cb8
nakster <g00332403@gmit.ie>
2019-03-02 21:50:04 +0000

    imports

--- TextRecognition/cnn.py
+++ TextRecognition/cnn.py
@@ -1,1 +1,10 @@
 #code for text i.e alphabet characters 
+##Import all the necessory imports 
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Dropout
+from keras.models import model_from_json
