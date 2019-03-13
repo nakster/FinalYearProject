b85aa077a708dabeb05bf3bb1baa855feb3ce68d
nakster <g00332403@gmit.ie>
2019-03-13 12:44:33 +0000

    reading in the file

--- ColourDectector/colourDetect.py
+++ ColourDectector/colourDetect.py
@@ -1,3 +1,36 @@
 import os
 import os.path
 import cv2
+import matplotlib.pyplot as plt
+
+
+#we going to read in the image that is going to be tested 
+print("Enter image (saved as colors e.g. black)")
+userInput = input()
+
+#this here reads the image in 
+image = cv2.imread('./Images/' + userInput + '.jpg')
+
+# Plot the image that we are going to test on 
+plt.figure("The Image to be Tested")
+plt.imshow(image) 
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# links 
+# https://stackoverflow.com/questions/3388223/python-difference-between-os-access-and-os-path-exists
+# https://stackoverflow.com/questions/35286540/display-an-image-with-python
