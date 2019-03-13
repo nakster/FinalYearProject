daf0d068762bb5469107882e3fb9f1e43915eb5f
nakster <g00332403@gmit.ie>
2019-03-13 12:47:42 +0000

    made it into a function

--- ColourDectector/colourDetect.py
+++ ColourDectector/colourDetect.py
@@ -3,18 +3,22 @@ import os.path
 import cv2
 import matplotlib.pyplot as plt
 
+def main():
+    #we going to read in the image that is going to be tested 
+    print("Enter image (saved as colors e.g. black)")
+    userInput = input()
 
-#we going to read in the image that is going to be tested 
-print("Enter image (saved as colors e.g. black)")
-userInput = input()
+    #this here reads the image in 
+    image = cv2.imread('./Images/' + userInput + '.jpg')
 
-#this here reads the image in 
-image = cv2.imread('./Images/' + userInput + '.jpg')
+    # Plot the image that we are going to test on 
+    plt.figure("The Image to be Tested")
+    plt.imshow(image) 
+    plt.show()
 
-# Plot the image that we are going to test on 
-plt.figure("The Image to be Tested")
-plt.imshow(image) 
-plt.show()
+
+#run the main method 
+main()
 
 
 
