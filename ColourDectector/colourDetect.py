d28296225d17cd59f39ea1d508d2e1f315c73888
nakster <g00332403@gmit.ie>
2019-03-14 15:27:52 +0000

    choice 1 working

--- ColourDectector/colourDetect.py
+++ ColourDectector/colourDetect.py
@@ -4,11 +4,19 @@ import cv2
 import matplotlib.pyplot as plt
 from API import KNN as classifier 
 from API import prepareTestImage as prepare
-
+from API import urlImage
 
 
 def main():
 
+    # Variables
+    # read the data file
+    # this is the path to the data training file
+    dataPath = './Data/training.data'
+    testPath = './Data/test.data'
+    prediction = ''
+    #test if the path is correct
+    # print(data)
     #make a countinous while loop 
     choice = True
 
@@ -29,71 +37,45 @@ def main():
             print("Enter image (saved as colors e.g. black)")
             userInput = input()
 
+             #this here reads the image in 
+            image = cv2.imread('./Images/' + userInput + '.jpg')
+
+            # os.path.isfile(path)
+            # Return True if path is an existing regular file. 
+            # This follows symbolic links, so both islink() and isfile() can be true for the same path.
+            # https://docs.python.org/2/library/os.path.html
+            if os.path.isfile(dataPath) and os.access(dataPath, os.R_OK):
+                print ('The Data File Exists!')
+            else:
+                print('Data File Does Not Exist!')
+
+            # this here is prepares the image to be tested 
+            # changes the test.data file to the rgb values of the image that is going to be tested 
+            prepare.prepareImage(image)
+        
+            # training the data 
+            prediction = classifier.main(dataPath, testPath)
+
+            # print the prediction 
+            print('The KNN predicts the Image to be: ' + prediction)
+
+            # Plot the image that we are going to test on 
+            # https://matplotlib.org/api/_as_gen/matplotlib.pyplot.figure.html#matplotlib.pyplot.figure
+            plt.figure("The Image to be Tested " + prediction)
+            # fixed color distortion error
+            # https://stackoverflow.com/questions/37795874/matplotlib-imshow-why-is-img-color-distorted    
+            plt.imshow(image[..., ::-1]) 
+            plt.show()
+
         #this is to test the image from the url    
         elif choice=="2":
-        
+            print("choice 2")
         # this is to exit the loop
         elif choice=="3":
             print("\n Goodbye") 
             choice = None
         else:
             print("\n Not Valid Choice Try again")
-
-
-  
-
-
-
-    
-
-    # response = requests.get(userInput)
-    # image = Image.open(BytesIO(response.content))
-
-    # image = Image.open(StringIO(urllib.requests.urlopen(userInput).read()))
-    #this here reads the image in 
-    # image = cv2.imread('./Images/' + userInput + '.jpg')
-    # image = cv2.imread(img)
-    plt.imshow(image[..., ::-1]) 
-    plt.show()
-
-
-    # Variables
-    # read the data file
-    # this is the path to the data training file
-    dataPath = './Data/training.data'
-    testPath = './Data/test.data'
-    prediction = ''
-
-    #test if the path is correct
-    # print(data)
-
-    # os.path.isfile(path)
-    # Return True if path is an existing regular file. 
-    # This follows symbolic links, so both islink() and isfile() can be true for the same path.
-    # https://docs.python.org/2/library/os.path.html
-    if os.path.isfile(dataPath) and os.access(dataPath, os.R_OK):
-        print ('The Data File Exists!')
-    else:
-        print('Data File Does Not Exist!')
-
-    # this here is prepares the image to be tested 
-    # changes the test.data file to the rgb values of the image that is going to be tested 
-    prepare.prepareImage(image)
-   
-    # training the data 
-    prediction = classifier.main(dataPath, testPath)
-
-    # print the prediction 
-    print('The KNN predicts the Image to be: ' + prediction)
-
-
-      # Plot the image that we are going to test on 
-    # https://matplotlib.org/api/_as_gen/matplotlib.pyplot.figure.html#matplotlib.pyplot.figure
-    plt.figure("The Image to be Tested " + prediction)
-    # fixed color distortion error
-    # https://stackoverflow.com/questions/37795874/matplotlib-imshow-why-is-img-color-distorted    
-    plt.imshow(image[..., ::-1]) 
-    plt.show()
 
 #run the main method 
 main()
