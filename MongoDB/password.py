57c311c339acde53d045ead4e201d8c63e4d0a03
nakster <g00332403@gmit.ie>
2019-03-18 18:01:31 +0000

    hash workin

--- MongoDB/password.py
+++ MongoDB/password.py
@@ -1,5 +1,7 @@
 # imports 
 from pymongo import MongoClient
+import hashlib
+import uuid
 
 # connecto to mongodb
 client = MongoClient("mongodb+srv://root:root@cluster0-xyrvy.mongodb.net/test?retryWrites=true")
@@ -8,6 +10,15 @@ db = client.User
 # name of the collection
 testCollection = db.passwords
 
+# hash the password 
+def hash_password(password):
+    # uuid is used to generate a random number
+    salt = uuid.uuid4().hex
+    return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
 
-
+# check the password entered with the saved password  
+def check_password(hashed_password, user_password):
+    password, salt = hashed_password.split(':')
+    return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
+ 
 
