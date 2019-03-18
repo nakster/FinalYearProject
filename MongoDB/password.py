50ad559dbad4070e4966a3121eec4e54333d9264
nakster <g00332403@gmit.ie>
2019-03-18 17:43:25 +0000

    make a new connection and collection

--- MongoDB/password.py
+++ MongoDB/password.py
@@ -1,2 +1,13 @@
 # imports 
 from pymongo import MongoClient
+
+# connecto to mongodb
+client = MongoClient("mongodb+srv://root:root@cluster0-xyrvy.mongodb.net/test?retryWrites=true")
+# tell which database you would like to use
+db = client.User
+# name of the collection
+testCollection = db.passwords
+
+
+
+
